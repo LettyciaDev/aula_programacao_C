63b7507da545c684967e99d605f9cf40ddcbd5b3
Lettycia Vitória <163659294+LettyciaDev@users.noreply.github.com>
2024-10-23 00:22:36 +0000

    lista

---
+++ lista/listaQuest1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+#define QTD 26
+
+typedef struct{
+    char nome[50];
+    int num_veiculo;
+    int acidente;
+}Estado;
+
+
+int main(){
+    Estado estados[QTD];
+
+}
+
+void lerEstado(struct Estado *estados) {
+    
+}
